ccbd52951bd12e3cda9d2ec8ea20eab4eef3fbb1
Ishtiaq Ahmed <ishtiaq.ahmed@strativ.se>
2024-10-06 21:45:36 +0600

    Add strativ inventory addon

---
+++ addons/strativ_inventory/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Strativ Inventory",
+    "summary": """
+        Inventory management app for Strativ""",
+    "description": """
+        This app is designed to streamline inventory management for Strativ.
+        It provides a centralized platform for tracking, organizing,
+        and managing assets, ensuring efficient operations and accurate record-keeping
+    """,
+    "author": "Strativ",
+    "website": "https://www.strativ.se",
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    "category": "Inventory/Inventory",
+    "version": "16.0.1.0.0",
+    # any module necessary for this one to work correctly
+    "depends": ["base"],
+    # always loaded
+    "data": [
+        # 'security/ir.model.access.csv',
+    ],
+    # only loaded in demonstration mode
+    "demo": [],
+}
