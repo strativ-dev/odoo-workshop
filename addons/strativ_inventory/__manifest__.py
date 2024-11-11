e2c1012fb3ff233fc50f01b134e10c3e7122f336
Tasnuva Tarannum Shreshtha <tasnuva.shreshtha@strativ.se>
2024-11-11 16:25:51 +0600

    list,form and search view of strativ inventory module

--- addons/strativ_inventory/__manifest__.py
+++ addons/strativ_inventory/__manifest__.py
@@ -9,7 +9,7 @@
         and managing assets, ensuring efficient operations and accurate record-keeping
     """,
     "author": "Strativ",
-    "website": "https://www.strativ.se",
+    # "website": "https://www.strativ.se",
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
@@ -19,8 +19,27 @@
     "depends": ["base"],
     # always loaded
     "data": [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
+        'views/items_views.xml',
+        'views/items_menus.xml'
     ],
     # only loaded in demonstration mode
     "demo": [],
+    'installable': True,
+    'application': True,
 }
+
+
+# {
+#     'name': 'Estate',
+#     'depends': [
+#         'base'
+#     ],
+#     "data": [
+#         'security/ir.model.access.csv',
+#         'views/estate_menus.xml',
+#         'views/estate_property_views.xml'
+#     ],
+#     'installable': True,
+#     'application': True,
+# }
--- addons/strativ_inventory/models/items.py
+++ addons/strativ_inventory/models/items.py
@@ -5,8 +5,19 @@ class Item(models.Model):
     _name = "strativ.inventory.item"
     _description = "Strativ Inventory Item"
 
-    name = fields.Char(string="Name", required=True)
+    name = fields.Char(string="Name")
     buy_price = fields.Float(string="Buy Price")
-    buy_date = fields.Date(string="Buy Date")
+    buy_date = fields.Date(string="Buy Date", default=fields.Datetime.now)
     description = fields.Text()
-    image = fields.Image(string="Image", max_width=1920, max_height=1920)
+    image = fields.Image(string="Image", max_width=1920, max_height=1920, copy=False)
+    active = fields.Boolean(string="Active", default=True)
+    state = fields.Selection(
+        selection=[
+            ('bought', 'Bought'),
+            ('pending', 'Pending'),
+            ('allocated', 'Allocated')
+        ],
+        string="State",
+        default='pending',
+        required=True
+    )
